2e080c6beeac6e03c80d89c9de85af9e2e04b360
JacksonLee <1602113077@qq.com>
2020-10-13 19:18:01 +0800

    oho

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,36 @@
 //logs.js
-const util = require('../../utils/util.js')
-
 Page({
-  data: {
-    logs: []
+  data:{
+    logs: [],
+    modalHidden: true,
+    toastHidden: true
   },
-  onLoad: function () {
+  onShow: function () {
+    var logs = wx.getStorageSync('todo_logs')
+    if (logs) {
+      this.setData({ logs: logs.reverse() })
+    }
+  },
+  set: function() {
+
+  },
+  onLoad: function() {},
+  switchModal: function() {
     this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+      modalHidden: !this.data.modalHidden
     })
+  },
+  hideToast: function() {
+    this.setData({
+      toastHidden: true
+    })
+  },
+  clearLog: function(e) {
+    wx.setStorageSync('logs', [])
+    this.switchModal()
+    this.setData({
+      toastHidden: false
+    })
+    this.onShow()
   }
 })
