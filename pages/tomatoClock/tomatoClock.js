adcf9107c31646f761181c0b2de994c104772f9e
JacksonLee <1602113077@qq.com>
2020-10-17 09:27:33 +0800

    ln

--- pages/tomatoClock/tomatoClock.js
+++ pages/tomatoClock/tomatoClock.js
@@ -1,66 +1,78 @@
-// pages/tomatoClock/tomatoClock.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    flag: true, 
+    disabled: false,  
+    opacity: 0.7,
+    settime: 25
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+ 
   onLoad: function (options) {
-
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  timeInput: function(e){
+    this.setData({
+      settime: parseInt(e.detail.value)
+    })
+  },
+
+  show: function () {
+    var that=this;
+    that.setData({flag: false });
+    countDown(that,25*60);
+  },
+
+  hide: function () {
+    this.setData({
+      flag: true,
+      disabled: true,
+      opacity: 0.7
+    })
+  },
+
   onReady: function () {
-
+    
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
-
+    
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
+    
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-
+    
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    
   },
+ 
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
-
+    
   }
 })
+ 
+function countDown(that, count) {
+  if (count == 0) {
+    that.setData({
+      counttime: count,
+      opacity:1
+    })
+    return;
+  }
+  that.setData({
+    counttime: count
+  })
+  setTimeout(function () {
+    count--;
+    countDown(that, count);
+  }, 1000);
+}
