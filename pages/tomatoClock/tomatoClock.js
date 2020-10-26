62948a0a76fee30fff3f9ded7574e68eaa0fce48
JacksonLee <1602113077@qq.com>
2020-10-26 22:43:09 +0800

    clock

--- pages/tomatoClock/tomatoClock.js
+++ pages/tomatoClock/tomatoClock.js
@@ -1,50 +1,52 @@
-var app = getApp()
 Page({
   data: {
-    flag: true, 
-    disabled: false,  
-    opacity: 0.7,
-    settime: 25
-  },
- 
-  onLoad: function (options) {
+   timer: '',
+   settime: 25,
+   counttime: 1500,
+   flag: true
   },
 
   timeInput: function(e){
     this.setData({
       settime: parseInt(e.detail.value)
     })
+    this.setData({
+      counttime: this.data.settime * 60
+    })
   },
 
-  show: function () {
-    var that=this;
-    that.setData({flag: false });
-    countDown(that,that.data.settime*60);
+  show: function() {
+    this.setData({
+      flag: false
+    })
+    this.countDown();
   },
 
   hide: function () {
     this.setData({
       flag: true,
-      disabled: false,
-      opacity: 0.7,
-      counttime:0
+      counttime: 0
     })
+    clearInterval(this.data.timer);
+  },
+
+  countDown: function () {
+   let that = this;
+   let counttime = that.data.settime * 60;
+   that.setData({
+    timer: setInterval(function () {
+     counttime--;
+     that.setData({
+      counttime: counttime
+     })
+     if (counttime == 0) {
+      clearInterval(that.data.timer);
+     }
+    }, 1000)
+   })
   }
-})
+ })
  
-function countDown(that, count) {
-  if (count == 0) {
-    that.setData({
-      counttime: count,
-      opacity:1
-    })
-    return;
-  }
-  that.setData({
-    counttime: count
-  })
-  setTimeout(function () {
-    count--;
-    countDown(that, count);
-  }, 1000);
-}
+
+
+
